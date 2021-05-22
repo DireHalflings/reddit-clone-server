0c650377df247c8d62ee601a1b46fbf401cb96ef
jjesusteran-mori <iokoumorit@gmail.com>
2021-05-22 13:30:20 -0700

    fixed minor error

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ const path = require("path");
 const { verifyToken } = require("./controllers/authController");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "client/build")));
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 const db = require("./config/database");
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(express.json());
 
